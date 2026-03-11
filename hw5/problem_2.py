e7cfcab6e402ee29f04b32e45aa4ef865e4034ce
Erdem <erdem@Phoebes-MacBook-Pro.local>
2026-03-12 01:49:04 +0800

    new hw

---
+++ hw5/problem_2.py
@@ -0,0 +1,65 @@
+class DoubleLinkedList:
+    class Node:
+        def __init__(self, element=None, prev=None, next=None):
+            self._element = element
+            self._prev = prev
+            self._next = next
+
+    def __init__(self):
+        self._header = DoubleLinkedList.Node(None, None, None)
+        self._trailer = DoubleLinkedList.Node(None, None, None)
+        self._header._next = self._trailer
+        self._trailer._prev = self._header
+        self._size = 0
+
+    def __len__(self):
+        return self._size
+
+    def is_empty(self):
+        return self._size == 0
+
+    def _insert_between(self, e, predecessor, successor):
+        newest = DoubleLinkedList.Node(e, predecessor, successor)
+        predecessor._next = newest
+        successor._prev = newest
+        self._size += 1
+        return newest
+
+    def _delete_node(self, node):
+        predecessor = node._prev
+        successor = node._next
+        predecessor._next = successor
+        successor._prev = predecessor
+        self._size -= 1
+        element = node._element
+        node._prev = node._next = node._element = None
+        return element
+
+    def __str__(self):
+        result = "Header<-->"
+        currNode = self._header._next
+        while currNode is not self._trailer:
+            result += str(currNode._element) + "<-->"
+            currNode = currNode._next
+        return result + "Trailer"
+
+    def rotate(self, n):
+        # TODO: Please write your code here
+        pass
+
+
+def main():
+    ls1 = DoubleLinkedList()
+    ls1._insert_between(5, ls1._header, ls1._header._next)
+    ls1._insert_between(4, ls1._header, ls1._header._next)
+    ls1._insert_between(3, ls1._header, ls1._header._next)
+    ls1._insert_between(2, ls1._header, ls1._header._next)
+    ls1._insert_between(1, ls1._header, ls1._header._next)
+
+    print(ls1)  # Should print: Header<-->1<-->2<-->3<-->4<-->5<-->Trailer
+    ls1.rotate(2)
+    print(ls1)  # Should print: Header<-->4<-->5<-->1<-->2<-->3<-->Trailer
+
+
+if __name__ == "__main__":
+    main()
